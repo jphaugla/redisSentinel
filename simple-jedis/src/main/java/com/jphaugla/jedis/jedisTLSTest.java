18d4e2e9529b9595d914f976f7c343281f92179d
Jason Haugland <jasonhaugland@gmail.com>
2023-04-27 18:02:35 -0500

    simplify and use environment variables

--- simple-jedis/src/main/java/com/jphaugla/jedis/jedisTLSTest.java
+++ simple-jedis/src/main/java/com/jphaugla/jedis/jedisTLSTest.java
@@ -12,18 +12,23 @@ public class jedisTLSTest {
     public static void main(String[] args) throws GeneralSecurityException, IOException {
         HostAndPort address = new HostAndPort("redis", 6379);
 
+        String trust_store = System.getenv("TRUSTSTORE_LOCATION");
+        String key_store = System.getenv("KEYSTORE_LOCATION");
+        String trust_store_pw = System.getenv("TRUSTSTORE_PASSWORD");
+        String key_store_pw = System.getenv("KEYSTORE_PASSWORD");
+        String redis_username = System.getenv("REDIS_USERNAME");
+        String redis_pw = System.getenv("REDIS_PASSWORD");
         SSLSocketFactory sslFactory = createSslSocketFactory(
-                "/usr/app/tls/client-truststore.jks",
-                "jasonrocks", // use the password you specified for keytool command
-                "/usr/app/tls/client-keystore.p12",
-                "jasonrocks" // use the password you specified for openssl command
+                trust_store,
+                trust_store_pw, // use the password you specified for keytool command
+                key_store,
+                key_store_pw // use the password you specified for openssl command
         );
 
         JedisClientConfig config = DefaultJedisClientConfig.builder()
-                .ssl(true)
-                .sslSocketFactory(sslFactory)
-                .user("jph") // use your Redis user. More info https://redis.io/docs/management/security/acl/
-                .password("jasonrocks") // use your Redis password
+                .ssl(true).sslSocketFactory(sslFactory)
+                .user(redis_username) // use your Redis user. More info https://redis.io/docs/management/security/acl/
+                .password(redis_pw) // use your Redis password
                 .build();
 
         JedisPooled jedis = new JedisPooled(address, config);
