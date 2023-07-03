aa9f0b4e0fc5d01570b4955c86c97e9f01b59aef
Eric Preston <eric.preston@redis.com>
2023-07-03 08:20:55 -0500

    updates for python

--- python/src/util/redis_connection.py
+++ python/src/util/redis_connection.py
@@ -12,3 +12,20 @@ class RedisConnection(object):
             port=os.getenv("REDIS_PORT", 6379),
             password=os.getenv("REDIS_PASSWORD", ""),
             encoding="utf-8", decode_responses=True)
+
+    @staticmethod
+    def get_client_tls():
+        ssl_certfile="/scripts/tests/tls/client.key"
+        ssl_keyfile="/scripts/tests/tls/client.crt"
+        ssl_ca_certs="/scripts/tests/tls/ca.crt"
+
+        return redis.StrictRedis(
+            host=os.getenv("REDIS_HOST", "localhost"),
+            port=os.getenv("REDIS_PORT", 6379),
+            password=os.getenv("REDIS_PASSWORD", ""),
+            ssl=True,
+            ssl_cert_reqs="required",
+            ssl_certfile=ssl_certfile,
+            ssl_keyfile=ssl_keyfile,
+            ssl_ca_certs=ssl_ca_certs,
+            encoding="utf-8", decode_responses=True)
